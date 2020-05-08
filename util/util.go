0ee54cc83f7f73de17c6245f10238d11924d2304
jitendra avinash <jitendra@jitendras-MacBook-Air.local>
2020-05-08 13:06:47 +0530

    removed unused code

--- util/util.go
+++ util/util.go
@@ -7,24 +7,6 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-func FindVendorByName(vendorName string) bool {
-	newVendor := db.Vendor{}
-	db.GetConnection().Collection("vendors").FindOne(context.TODO(), bson.D{{"name", vendorName}}).Decode(&newVendor)
-
-	// return true if a document is present and
-	// false if the vendor is new vendor
-	return false
-}
-
-func FindVendorById(vendorId primitive.ObjectID) bool {
-	vendorFromDB := db.Vendor{}
-	db.GetConnection().Collection("vendors").FindOne(context.TODO(), bson.D{{"_id", vendorId}}).Decode(&vendorFromDB)
-
-	// return true if a document is present and
-	// false if the vendor is new vendor
-	return false
-}
-
 func FindItemById(itemId primitive.ObjectID) bool {
 	itemFromDB := db.Item{}
 	db.GetConnection().Collection("items").FindOne(context.TODO(), bson.D{{"_id", itemId}}).Decode(&itemFromDB)
